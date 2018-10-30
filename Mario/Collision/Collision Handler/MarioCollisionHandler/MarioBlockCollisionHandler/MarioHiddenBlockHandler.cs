598115bdfc3c7e5cdaf66b124bccc720dd9ee613
Ji Woong Kim <greenyjwk@gmail.com>
2018-10-29 23:53:03 -0400

    code review

--- Mario/Collision/Collision Handler/MarioCollisionHandler/MarioBlockCollisionHandler/MarioHiddenBlockHandler.cs
+++ Mario/Collision/Collision Handler/MarioCollisionHandler/MarioBlockCollisionHandler/MarioHiddenBlockHandler.cs
@@ -12,20 +12,21 @@ namespace Mario.Collision.MarioCollisionHandler.MarioBlockCollisionHandler
     public class MarioHiddenBlockHandler : IMarioCollisionHandler
     {
         Rectangle intersection;
-        public MarioHiddenBlockHandler(Rectangle intersection)
-		{
+        public MarioHiddenBlockHandler(IBlock block, Rectangle intersection)
+        {
+            this.block = block;
             this.intersection = intersection;
         }
         public void HandleCollision(IMario mario, Direction result)
         {
             PoisitionAdjustment(mario, result, intersection);
         }
-        public static void PoisitionAdjustment(IMario mario, Direction result, Rectangle intersection)
+        public static void PoisitionAdjustment(IMario marioParam, Direction result, Rectangle intersection)
         {
             if (result==Direction.Down)
             {
-                mario.Position += Vector2.UnitY * intersection.Height;
-                mario.Physics.ResetGravity();
+                marioParam.Position += Vector2.UnitY * intersection.Height;
+                marioParam.Physics.ResetGravity();
             }
         }
     }
