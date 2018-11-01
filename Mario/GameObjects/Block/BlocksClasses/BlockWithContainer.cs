fa15403f9bfe4934fb13e1e0d1b37e66a751b2f4
Jacob Grove <grove.217@osu.edu>
2018-11-01 12:53:29 -0400

    Beggining block class changes

---
+++ Mario/GameObjects/Block/BlocksClasses/BlockWithContainer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Game1;
+namespace Mario.Classes.BlocksClasses
+{
+	internal class BlockWithContainer : Block
+	{
+		public BlockWithContainer(Vector2 location) : base(location)
+		{
+		}
+
+		public override Rectangle Box => base.Box;
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			base.Draw(spriteBatch);
+		}
+
+		public override void React()
+		{
+			BlockState.React();
+		}
+
+		public override void SetContainsItem(string item)
+		{
+			base.SetContainsItem(item);
+		}
+
+		public override void Update()
+		{
+			base.Update();
+		}
+	}
+}
