b77e00acb6186c8222825758d7922984cb265f6c
Jacob Grove <grove.217@osu.edu>
2018-11-13 16:16:40 -0500

    classes for sliding down flag and other events

---
+++ Mario/GameObjects/Decorators/Special Event Behaviors/SlideDownFlagDecorator.cs
@@ -0,0 +1,36 @@
+﻿using Game1;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mario.GameObjects.Decorators.Special_Event_Behaviors
+{
+	class SlideDownFlagDecorator :MarioSpecialEventDecorator
+	{
+		private Vector2 locationOfBase = Vector2.Zero;
+		private float timeToFall = 1;
+
+
+		public SlideDownFlagDecorator(IMario mario, Vector2 locationOfBase, float timeToFall):base(mario)
+		{
+			this.locationOfBase = locationOfBase;
+			this.timeToFall = timeToFall;
+		}
+
+		public override void Update()
+		{
+			while(Math.Abs(DecoratedMario.Position.Y - locationOfBase.Y) > 1)
+			{
+				DecoratedMario.Position += Vector2.UnitY * 10;
+			}
+		}
+
+		public override void RemoveSelf()
+		{
+			base.RemoveSelf();
+		}
+	}
+}
